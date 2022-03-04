4d897cd3c5c8282af9e6d98e5c9df0760bd9c11c
서녕이 <@DESKTOP-THJ3TS8>
2022-03-04 10:39:50 +0900

    0304

--- Petmily/src/main/java/com/petmily/service/OrderService.java
+++
@@ -1,7 +0,0 @@
-package com.petmily.service;
-
-import com.petmily.dto.Order;
-
-public interface OrderService {
-	void order(Order order) throws Exception;
-}
--- Petmily/src/main/java/com/petmily/service/UserService.java
+++ Petmily/src/main/java/com/petmily/service/UserService.java
@@ -1,9 +1,6 @@
 package com.petmily.service;
 
-import com.petmily.dto.User;
 
 public interface UserService {
-   void join(User user) throws Exception;
-   boolean login(String user_id, String user_pwd) throws Exception;
-   boolean joinOverlap(String id) throws Exception;
+  
 }
